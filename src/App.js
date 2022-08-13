54fdf5953f587c07a73d82b958fd94066f1534d8
Madison Funderburk <madfun12@gmail.com>
2022-08-13 09:24:31 -0500

    adding sass, dice, functions to change them

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-
+import React from 'react'
+import Die from './components/Die'
 function App() {
+  const [dice, setDice] = React.useState(allNewDice())
+
+  function allNewDice(){
+    let arrayOfDice = []
+    for(let i=0;i<10;i++){
+      arrayOfDice.push(Math.floor(Math.random() * 6 + 1))
+    }
+    return arrayOfDice
+  }
+
+  function getNewDice(){
+    setDice(allNewDice)
+  }
+
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-    </div>
+    <main>
+      <div className="die-container">
+        {dice.map(item => {
+          return(
+            <Die value={dice[item]} />
+          )
+        })}
+      </div>
+      <button onClick={getNewDice}>Roll</button>
+      
+    </main>
   );
 }
 
---
+++ src/components/Die.js
@@ -0,0 +1,5 @@
+export default function Die(props){
+    return(
+        <div className="die"><h2>{props.value}</h2></div>
+    )
+}
