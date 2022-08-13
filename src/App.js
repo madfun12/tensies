6c7dafca01a665c0e3c4088592234c8d88ff3973
Madison Funderburk <madfun12@gmail.com>
2022-08-13 10:26:50 -0500

    conditional styling

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { getQueriesForElement } from '@testing-library/react'
 import React from 'react'
 import Die from './components/Die'
 function App() {
@@ -6,25 +7,35 @@ function App() {
   function allNewDice(){
     let arrayOfDice = []
     for(let i=0;i<10;i++){
-      arrayOfDice.push(Math.floor(Math.random() * 6 + 1))
+      arrayOfDice[i]= {
+        id: i,
+        value : Math.floor(Math.random() * 6 + 1),
+        isFrozen : true
+      }
     }
     return arrayOfDice
   }
 
-  function getNewDice(){
+  function rollDice(){
     setDice(allNewDice)
   }
 
+  
+
   return (
     <main>
+      <div className="info">
+        <h1>Tensies!</h1>
+        <p>Roll the dice until they all have the same number. You can click a die to freeze it.</p>
+      </div>
       <div className="die-container">
         {dice.map(item => {
           return(
-            <Die value={dice[item]} />
+            <Die value={item.value} key={item.id} isFrozen={item.isFrozen}/>
           )
         })}
       </div>
-      <button onClick={getNewDice}>Roll</button>
+      <button onClick={rollDice}>Roll</button>
       
     </main>
   );
--- src/components/Die.js
+++ src/components/Die.js
@@ -1,5 +1,8 @@
 export default function Die(props){
+    const styles={
+        backgroundColor: props.isFrozen ? "#59E391" : "#fff"
+    }
     return(
-        <div className="die"><h2>{props.value}</h2></div>
+        <div className="die" style={styles} ><h2>{props.value}</h2></div>
     )
 }
